507635c2a407011bd798cab2938f2b41c4c60f9c
김현민 <gimhyeonmin@gimhyeonmin-ui-MacBookPro.local>
2025-07-23 21:23:41 +0900

    Add 1RM calculator fronted and static server

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 app.get('/', (req, res) => {
